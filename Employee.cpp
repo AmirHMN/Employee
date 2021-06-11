77f37618a24999aa4d2e8b7ab3842cbb95b2aff5
AmirHmn <amir.hosein6979@gmail.com>
2021-06-11 15:57:32 +0430

    copy constructor added (employee class)

--- Employee.cpp
+++ Employee.cpp
@@ -1,4 +1,16 @@
 #include "Employee.h"
 
-Employee::Employee(const string &name, const string &id, const Address &address, int hourWork, int salaryPerHour , int workToDo , int workDone)
-        : Person(name, id, address), hourWork(hourWork), salaryPerHour(salaryPerHour) , wordToDo(workToDo) , workDone(workDone) {}
+//constructor
+Employee::Employee(const string &name, const string &id, const Address &address, int hourWork, int salaryPerHour,
+                   int workToDo, int workDone)
+        : Person(name, id, address), hourWork(hourWork), salaryPerHour(salaryPerHour), workToDo(workToDo),
+          workDone(workDone) {}
+
+
+//copy constructor
+Employee::Employee(const Employee &employee) : Person(employee) {
+    this->hourWork = employee.hourWork;
+    this->salaryPerHour = employee.salaryPerHour;
+    this->workDone = employee.workDone;
+    this->workToDo = employee.workToDo;
+}
--- Employee.h
+++ Employee.h
@@ -1,17 +1,22 @@
 #include "Person.h"
+
 #ifndef ADDRESS_EMPLOYEE_H
 #define ADDRESS_EMPLOYEE_H
 
 
-class Employee : public Person{
+class Employee : public Person {
 private:
     int hourWork;
     int salaryPerHour;
-    int wordToDo;
+    int workToDo;
     int workDone;
 public:
     // constructor
-    Employee(const string &name, const string &id, const Address &address, int hourWork, int salaryPerHour , int workToDo , int workDone);
+    Employee(const string &name, const string &id, const Address &address, int hourWork, int salaryPerHour,
+             int workToDo, int workDone);
+
+    //copy constructor
+    Employee(const Employee & employee);
 };
 
 
